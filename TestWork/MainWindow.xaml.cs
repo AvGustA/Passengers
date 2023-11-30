98008e979f7422c9749c57d8bf6661b1e3ce670c
AvGusta <AvGusta@DESKTOP-PINERB3>
2023-11-30 10:37:48 +0300

    Add project files.

---
+++ TestWork/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using TestWork.Models;
+
+namespace TestWork
+{
+    public partial class MainWindow : Window
+    {
+        private const string FileName = "Passengers.json";
+        private ObservableCollection<Passenger> passengers = new ObservableCollection<Passenger>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (File.Exists(FileName))
+            {
+                var json = File.ReadAllText(FileName);
+                passengers = JsonSerializer.Deserialize<ObservableCollection<Passenger>>(json);
+            }
+            dataGrid.ItemsSource = passengers;
+        }
+
+        private void MyDataGrid_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            if (e.EditAction == DataGridEditAction.Commit)
+            {
+                var bindingGroup = e.Row.BindingGroup;
+                if (bindingGroup != null && bindingGroup.CommitEdit())
+                {
+                    var json = JsonSerializer.Serialize(passengers);
+                    File.WriteAllText(FileName, json);
+                }
+            }
+        }
+    }
+}
---
+++ TestWork/Models/Passenger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using System.Windows.Markup;
+
+namespace TestWork.Models
+{
+    class Passenger
+    {
+        /// <summary>
+        /// Номер рейса
+        /// </summary>
+        public string FlightNumber { get; set; }
+
+        /// <summary>
+        /// ФИО пассажира
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Время отправки самолёта
+        /// </summary>
+        public DateTime DepartureTime { get; set; }
+    }
+}
